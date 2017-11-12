df075373670afdeb2675973ce90f9e955d24ec52
Elise Fu <elise@elisefu.com>
2017-11-12 09:29:29 -0500

    Checkpoint 5 Controllers: Completed

---
+++ app/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,9 @@
+(function(){
+    function LandingCtrl() {
+    }
+    
+    angular
+        .module('blocJams')
+        .controller('LandingCtrl', LandingCtrl);
+            this.heroTitle = "Turn the Music Up!";
+})();
